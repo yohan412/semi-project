dacb2b441400c0537f8dfa0510a643e1bc091e08
yohan <sdjadygks@naver.com>
2021-10-21 11:33:28 +0900

    유저 서블렛

---
+++ src/com/user/controller/UserController.java
@@ -0,0 +1,38 @@
+package com.user.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/usercontroller")
+public class UserController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html charset=UTF-8");
+		
+		String command = request.getParameter("command");
+		System.out.println("[ command : " + command + "]");
+		
+		if(command.equals("loginform")) {
+			dispatch("login.jsp",request,response);
+		}
+	}
+	
+	private void dispatch(String url, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher dis = request.getRequestDispatcher(url);
+		dis.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
+

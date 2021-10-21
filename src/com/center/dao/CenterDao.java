c46cda3c8ccab0b0c21bcc10fc483841de1c66ac
jiyoungbb <jiiyoung0906@naver.com>
2021-10-21 12:31:16 +0900

    center dao, dto  JDBCTemplate추가

---
+++ src/com/center/dao/CenterDao.java
@@ -0,0 +1,20 @@
+package com.center.dao;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.center.dto.CenterDto;
+import common.JDBCTemplate;
+
+
+public class CenterDao {
+
+	//센터 목록
+	public List<CenterDto> selectAll(){
+		
+		return null;
+	}
+}

1874b8d476dbc9a23321df5523343e21445dcff2
jwcom1995 <95god95@gmail.com>
2021-10-26 14:46:00 +0900

    UsedDao, UsedDto, UsedController 클래스 생성

---
+++ src/com/used/dao/UsedDao.java
@@ -0,0 +1,59 @@
+package com.used.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.used.dto.UsedDto;
+
+import common.JDBCTemplate;
+
+public class UsedDao extends JDBCTemplate{
+	
+	public List<UsedDto> selectAll(){
+		Connection con = getConnection();
+		PreparedStatement pstm = null;
+		ResultSet rs = null;
+		List<UsedDto> res = new ArrayList<UsedDto>();
+		
+		String sql = "SELECT * FROM USED_BOARD";
+		
+		try {
+			pstm=con.prepareStatement(sql);
+			System.out.println("03. query 준비: " + sql);
+			
+			rs=pstm.executeQuery();
+			System.out.println("04. query 실행 및 리턴");
+			
+			while(rs.next()) {
+				UsedDto dto = new UsedDto();
+				
+				dto.setUsedno(rs.getInt(1));
+				dto.setUsedtitle(rs.getString(2));
+				dto.setUserid(rs.getString(3));
+				dto.setUserno(rs.getInt(4));
+				dto.setUsedaddr(rs.getString(5));
+				dto.setUsedprice(rs.getString(6));
+				dto.setUsedcategory(rs.getString(7));
+				dto.setUsedpic(rs.getString(8));
+				dto.setUsedcenternm(rs.getString(9));
+				dto.setUsedcontent(rs.getString(10));
+				dto.setUsedreg(rs.getDate(11));
+				dto.setUsedstatus(rs.getString(12));
+				
+				res.add(dto);
+			}
+		} catch (SQLException e) {
+			System.out.println("3/4 단계 오류");
+			e.printStackTrace();
+		} finally {
+			close(rs);
+			close(pstm);
+			close(con);
+		}
+		return res;
+	}
+}
---
+++ src/com/used/dto/UsedDto.java
@@ -0,0 +1,140 @@
+package com.used.dto;
+
+import java.util.Date;
+
+public class UsedDto {
+	
+	private int usedno;
+	private String usedtitle;
+	private String userid;
+	private int userno;
+	private String usedaddr;
+	private String usedprice;
+	private String usedcategory;
+	private String usedpic;
+	private String usedcenternm;
+	private String usedcontent;
+	private Date usedreg;
+	private String usedstatus;
+	
+	public UsedDto() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public UsedDto(int usedno, String usedtitle, String userid, int userno, String usedaddr, String usedprice,
+			String usedcategory, String usedpic, String usedcenternm, String usedcontent, Date usedreg,
+			String usedstatus) {
+		super();
+		this.usedno = usedno;
+		this.usedtitle = usedtitle;
+		this.userid = userid;
+		this.userno = userno;
+		this.usedaddr = usedaddr;
+		this.usedprice = usedprice;
+		this.usedcategory = usedcategory;
+		this.usedpic = usedpic;
+		this.usedcenternm = usedcenternm;
+		this.usedcontent = usedcontent;
+		this.usedreg = usedreg;
+		this.usedstatus = usedstatus;
+	}
+
+	public int getUsedno() {
+		return usedno;
+	}
+
+	public void setUsedno(int usedno) {
+		this.usedno = usedno;
+	}
+
+	public String getUsedtitle() {
+		return usedtitle;
+	}
+
+	public void setUsedtitle(String usedtitle) {
+		this.usedtitle = usedtitle;
+	}
+
+	public String getUserid() {
+		return userid;
+	}
+
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+
+	public int getUserno() {
+		return userno;
+	}
+
+	public void setUserno(int userno) {
+		this.userno = userno;
+	}
+
+	public String getUsedaddr() {
+		return usedaddr;
+	}
+
+	public void setUsedaddr(String usedaddr) {
+		this.usedaddr = usedaddr;
+	}
+
+	public String getUsedprice() {
+		return usedprice;
+	}
+
+	public void setUsedprice(String usedprice) {
+		this.usedprice = usedprice;
+	}
+
+	public String getUsedcategory() {
+		return usedcategory;
+	}
+
+	public void setUsedcategory(String usedcategory) {
+		this.usedcategory = usedcategory;
+	}
+
+	public String getUsedpic() {
+		return usedpic;
+	}
+
+	public void setUsedpic(String usedpic) {
+		this.usedpic = usedpic;
+	}
+
+	public String getUsedcenternm() {
+		return usedcenternm;
+	}
+
+	public void setUsedcenternm(String usedcenternm) {
+		this.usedcenternm = usedcenternm;
+	}
+
+	public String getUsedcontent() {
+		return usedcontent;
+	}
+
+	public void setUsedcontent(String usedcontent) {
+		this.usedcontent = usedcontent;
+	}
+
+	public Date getUsedreg() {
+		return usedreg;
+	}
+
+	public void setUsedreg(Date usedreg) {
+		this.usedreg = usedreg;
+	}
+
+	public String getUsedstatus() {
+		return usedstatus;
+	}
+
+	public void setUsedstatus(String usedstatus) {
+		this.usedstatus = usedstatus;
+	}
+	
+	
+}

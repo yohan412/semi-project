55dcf1551897a6189763ba2166f50d2fa509d228
jwcom1995 <87286684+jwcom1995@users.noreply.github.com>
2021-10-30 15:33:45 +0900

    UsedDao, UsedDto pic 컬럼 없는버젼으로 수정

--- src/com/used/dao/UsedDao.java
+++ src/com/used/dao/UsedDao.java
@@ -38,11 +38,10 @@ public class UsedDao extends JDBCTemplate{
 				dto.setUsedaddr(rs.getString(5));
 				dto.setUsedprice(rs.getString(6));
 				dto.setUsedcategory(rs.getString(7));
-				dto.setUsedpic(rs.getString(8));
-				dto.setUsedcenternm(rs.getString(9));
-				dto.setUsedcontent(rs.getString(10));
-				dto.setUsedreg(rs.getDate(11));
-				dto.setUsedstatus(rs.getString(12));
+				dto.setUsedcenternm(rs.getString(8));
+				dto.setUsedcontent(rs.getString(9));
+				dto.setUsedreg(rs.getDate(10));
+				dto.setUsedstatus(rs.getString(11));
 				
 				res.add(dto);
 			}
@@ -82,11 +81,10 @@ public class UsedDao extends JDBCTemplate{
 				res.setUsedaddr(rs.getString(5));
 				res.setUsedprice(rs.getString(6));
 				res.setUsedcategory(rs.getString(7));
-				res.setUsedpic(rs.getString(8));
-				res.setUsedcenternm(rs.getString(9));
-				res.setUsedcontent(rs.getString(10));
-				res.setUsedreg(rs.getDate(11));
-				res.setUsedstatus(rs.getString(12));
+				res.setUsedcenternm(rs.getString(8));
+				res.setUsedcontent(rs.getString(9));
+				res.setUsedreg(rs.getDate(10));
+				res.setUsedstatus(rs.getString(11));
 				
 			}
 		} catch (SQLException e) {

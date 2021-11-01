8fa0879de75d6e5fabbff000dcfeb9dc68bc5161
jwcom1995 <95god95@gmail.com>
2021-11-01 12:48:33 +0900

    게시글 거래상태 변경 기능 추가 / 1:1 문의게시글 답변할 경우 답변상태 변경

--- src/com/used/dao/UsedDao.java
+++ src/com/used/dao/UsedDao.java
@@ -204,4 +204,35 @@ public class UsedDao extends JDBCTemplate{
 		}
 		return res;
 	}
+	
+	public int updateStatus (int usedno, String status) {
+		Connection con = getConnection();
+		PreparedStatement pstm = null;
+		int res = 0;
+		
+		String sql = "UPDATE USED_BOARD SET USED_STATUS=? WHERE USED_NO=?";
+		try {
+			pstm=con.prepareStatement(sql);
+			pstm.setString(1, status);
+			pstm.setInt(2, usedno);
+			System.out.println("03. query 준비 : "+sql);
+			
+			res=pstm.executeUpdate();
+			System.out.println("04. query 실행 및 리턴");
+			
+			if(res>0) {
+				commit(con);
+			}else {
+				rollback(con);
+			}
+		} catch (SQLException e) {
+			System.out.println("error : 3/4 단계 failed");
+			e.printStackTrace();
+		} finally {
+			close(pstm);
+			close(con);
+			System.out.println("05. db 종료 \n");
+		}
+		return res;
+	}
 }

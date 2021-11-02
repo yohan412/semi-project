b94b5dc6a0660a652660cd2adacd62ad86a8c45e
YEJI <yedoong919@gmail.com>
2021-11-02 14:07:01 +0900

    qna 1대1문의 페이징 설정 중(미완)

--- src/com/qna/dao/QnaDao.java
+++ src/com/qna/dao/QnaDao.java
@@ -7,6 +7,12 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+
+
 import com.qna.dto.QnaDto;
 
 import common.JDBCTemplate;
@@ -98,6 +104,7 @@ public class QnaDao extends JDBCTemplate{
 				}
 				return res;
 			}
+			
 			public int insert(QnaDto dto) {
 				Connection con = getConnection();
 				PreparedStatement pstm = null;
@@ -208,4 +215,144 @@ public class QnaDao extends JDBCTemplate{
 				}
 				return res;
 			}
+			
+			
+			
+////////////////////////////////////////////////////////////////////////
+	// 싱글톤
+	private static QnaDao instance = new QnaDao();
+	
+	public static QnaDao getInstance() {
+			return instance;
+	}
+
+	public List<QnaDto> getList() {
+		Connection con = getConnection();
+		PreparedStatement pstm = null;
+		ResultSet rs = null;
+		List<QnaDto> res = null;
+		
+		String sql = "select * from qna order by qano desc";
+		
+		try {
+			pstm = con.prepareStatement(sql); 
+			System.out.println("03.query 준비: " + sql);
+			
+			rs = pstm.executeQuery(); 
+			System.out.println("04.query 실행 및 리턴");
+			
+			if (rs.next()) { 
+				res = new ArrayList<>(); 
+				do {
+					QnaDto dto = new QnaDto();
+					dto.setQano(rs.getInt("qano"));
+					dto.setQagpno(rs.getInt("qagpno"));
+					dto.setQagpsq(rs.getInt("qagpsq"));
+					dto.setUserid(rs.getString("userid"));
+					dto.setUserno(rs.getInt("userno"));
+					dto.setQatitle(rs.getString("qatitle"));
+					dto.setQacontent(rs.getString("qacontent"));
+					dto.setQafaq(rs.getString("qafaq"));
+					dto.setQareg(rs.getDate("qareg"));
+					dto.setQastatus(rs.getString("qastatus"));
+					
+					res.add(dto); 
+					
+				} while (rs.next());
+			}
+		} catch (Exception e) {
+			System.out.println("3/4 단계 에러");
+			e.printStackTrace();
+			
+		} finally {
+			close(pstm);
+			close(con);
+			System.out.println("05.db 종료\n");
+		}
+		return res;
+	}
+
+	// qna.jsp에 보여줄 로직 (페이징 처리)
+		public List<QnaDto> getList(int startRow, int endRow){
+			Connection con = getConnection();
+			PreparedStatement pstm = null;
+			ResultSet rs = null;
+			List<QnaDto> res = null;
+				
+			String sql = "select * from "
+					+ "(select rownum rn, qano, qagpno, qagpsq, userid, userno, qatype, qatitle, qacontent, qafaq, qareg, qastatus from "
+					+ "(select * from qna order by qano desc)) where rn between ? and ?";
+				
+			try {
+				pstm = con.prepareStatement(sql); // sql 정의
+				pstm.setInt(1, startRow); // sql 물음표에 값 매핑
+				pstm.setInt(2, endRow);
+				System.out.println("03.query 준비: " + sql);
+					
+				rs = pstm.executeQuery(); // sql 실행
+				System.out.println("04.query 실행 및 리턴");
+				
+				if (rs.next()) { // 데이터베이스에 데이터가 있으면 실행
+					res = new ArrayList<>(); // list 객체 생성
+					do {
+					// 반복할 때마다 QnaDto 객체를 생성 및 데이터 저장
+						QnaDto dto = new QnaDto();
+						dto.setQano(rs.getInt("qano"));
+						dto.setQagpno(rs.getInt("qagpno"));
+						dto.setQagpsq(rs.getInt("qagpsq"));
+						dto.setUserid(rs.getString("userid"));
+						dto.setUserno(rs.getInt("userno"));
+						dto.setQatitle(rs.getString("qatitle"));
+						dto.setQacontent(rs.getString("qacontent"));
+						dto.setQafaq(rs.getString("qafaq"));
+						dto.setQareg(rs.getDate("qareg"));
+						dto.setQastatus(rs.getString("qastatus"));
+						
+						res.add(dto); // res에 0번 인덱스부터 board 객체의 참조값을 저장
+							
+					} while (rs.next());
+				}
+			} catch (Exception e) {
+				System.out.println("3/4 단계 에러");
+				e.printStackTrace();
+					
+			} finally {
+				close(pstm);
+				close(con);
+				System.out.println("05.db 종료\n");
+			}
+			return res;
+		}
+			
+	// 총 레코드 수 구하는 로직
+		public int getCount(){
+			Connection con = getConnection();
+			PreparedStatement pstm = null;
+			ResultSet rs = null;
+			int count = 0;
+				
+			String sql = "select count(*) from qna ";
+			try {
+				pstm = con.prepareStatement(sql);
+				System.out.println("03.query 준비: " + sql);
+				
+				rs = pstm.executeQuery();
+				System.out.println("04.query 실행 및 리턴");
+					
+				if(rs.next()){
+					count = rs.getInt(1);
+				}
+					
+			} catch (Exception e) {
+				System.out.println("3/4 단계 에러");
+				e.printStackTrace();
+					
+			} finally {
+				close(rs);
+				close(pstm);
+				close(con);
+				System.out.println("05.db 종료\n");
+			}	
+			return count; // 총 레코드 수 리턴
+		}
 }

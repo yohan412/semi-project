0314b620afc945874c1dc0b401089a50b95adef9
jwcom1995 <95god95@gmail.com>
2021-11-05 11:13:04 +0900

    UsedDao 작성자에 따른 selectWriter 추가

--- src/com/used/dao/UsedDao.java
+++ src/com/used/dao/UsedDao.java
@@ -529,4 +529,49 @@ public int insertWish(WishDto dto) {
 		}
 		return res;
 	}
+	
+	public List<UsedDto> selectWriter(String userid){
+		Connection con = getConnection();
+		PreparedStatement pstm = null;
+		ResultSet rs = null;
+		List<UsedDto> res = new ArrayList<UsedDto>();
+		
+		String sql = "SELECT * FROM USED_BOARD WHERE USER_ID=?";
+		
+		try {
+			pstm=con.prepareStatement(sql);
+			pstm.setString(1, userid);
+			System.out.println("03. query 준비: " + sql);
+			
+			rs=pstm.executeQuery();
+			System.out.println("04. query 실행 및 리턴");
+			
+			while(rs.next()) {
+				UsedDto dto = new UsedDto();
+				
+				dto.setUsedno(rs.getInt(1));
+				dto.setUsedtitle(rs.getString(2));
+				dto.setUserid(rs.getString(3));
+				dto.setUserno(rs.getInt(4));
+				dto.setUsedaddr(rs.getString(5));
+				dto.setUsedprice(rs.getString(6));
+				dto.setUsedcategory(rs.getString(7));
+				dto.setUsedcenternm(rs.getString(8));
+				dto.setUsedcontent(rs.getString(9));
+				dto.setUsedreg(rs.getDate(10));
+				dto.setUsedstatus(rs.getString(11));
+				
+				res.add(dto);
+			}
+		} catch (SQLException e) {
+			System.out.println("3/4 단계 오류");
+			e.printStackTrace();
+		} finally {
+			close(rs);
+			close(pstm);
+			close(con);
+			System.out.println("05. db 종료\n");
+		}
+		return res;
+	}
 }
